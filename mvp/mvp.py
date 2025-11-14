d1f08ae9c8a67633ac412d727c08386be92885fe
alejandronava562 <navaalejandro562@gmail.com>
2025-11-13 19:28:42 -0800

    working on idea printing

--- mvp/mvp.py
+++ mvp/mvp.py
@@ -1,19 +1,42 @@
 from openai import OpenAI
+from dotenv import load_dotenv
+load_dotenv()
 import json
 import os
-from prompts import USER_TEMPLATE, SYSTEM_PROMPT
+from prompts import USER_TEMPLATE, SYSTEM_PROMPT, IDEA_SCHEMA
 
-# API_KEY = os.getenv("OPENAI_API_KEY")
+api_key=os.getenv("OPENAI_API_KEY")
 
-# client = OpenAI(
-#      api_key = API_KEY
-# )
+client = OpenAI(api_key=api_key)
+
+def call_api(system_prompt, user_prompt, schema):
+    response = client.chat.completions.create(
+    model="gpt-5-mini",
+    messages=[
+        {"role": "user", "content": user_prompt},
+        {"role": "system", "content": system_prompt}
+    ],
+    response_format={"type": "json_schema", "json_schema": schema}
+    )
+    text = response.choices[0].message.content
+    return json.loads(text) # type: ignore
+
 
 def main():
     print("app")
     
-    user_context = input("Describe the context project that you will be working on.")
-    user_project = input("Describe the project.")  
+    user_project = input("Describe the project.").strip()
+    user_context = input("Describe the context of the project that you will be working on.").strip()
+
+    print("Generating possible ideas...\n")
+    ideas = call_api(SYSTEM_PROMPT, 
+                     USER_TEMPLATE.format(project=user_project, context= user_context), 
+                     IDEA_SCHEMA)
+    print(ideas["summary"])
+    print("IDEAS:\n")
+    for idea in ideas["direction"]:
+        
+
 
 if __name__ == "__main__":
     main()
--- mvp/prompts.py
+++ mvp/prompts.py
@@ -7,17 +7,42 @@ User profile:
 - project : {project}
 - context : {context}
 
-Chosen idea (from prior options):
-{chosen_idea_json}
-
 Task:
 1) Identify the main issue of the project
 2) Propose 5 distinct ideas that could help the user start
 3) Each idea must have:
-{
+{{
     "ID": "ID_NUMBER",
     "LABEL": "SHORT_READABLE_NAME", 
     "TIMELINE": "SIMPLE_TIMELINE",
-}
+}}
 Return only a JSON that matches the template
 """
+
+IDEA_SCHEMA = {
+  "name": "writers_ideation_output",
+  "schema": {
+    "type": "object",
+    "properties": {
+      "summary": {"type": "string"},
+      "directions": {
+        "type": "array",
+        "items": {
+          "type": "object",
+          "properties": {
+            "idea_num": {"type": "string"},
+            "label": {"type": "string"},
+            "description": {"type": "string"},
+            "sample_prompt": {"type": "string"},
+            "sample_outline": {"type": "string"}
+          },
+          "required": ["idea_num", "label", "description", "sample_prompt", "sample_outline"],
+          "additionalProperties": False
+        }
+      }
+    },
+    "required": ["summary", "directions"],
+    "additionalProperties": False
+  },
+  "strict": True
+}
