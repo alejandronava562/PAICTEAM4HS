34d77180e0842a7265a2bab0c0011c07cea08f27
alejandronava562 <navaalejandro562@gmail.com>
2025-11-20 19:29:42 -0800

    plan creation WIP

--- mvp/mvp.py
+++ mvp/mvp.py
@@ -1,39 +1,46 @@
 from openai import OpenAI
 from dotenv import load_dotenv
+
 load_dotenv()
 import json
 import os
-from prompts import USER_TEMPLATE, SYSTEM_PROMPT, IDEA_SCHEMA
+from prompts import USER_TEMPLATE, SYSTEM_PROMPT, IDEA_SCHEMA, USER_PLAN, CHOSEN_PLAN_SCEMA
 
-api_key=os.getenv("OPENAI_API_KEY")
+api_key = os.getenv("OPENAI_API_KEY")
 
 client = OpenAI(api_key=api_key)
 
 # Alejandro
 
+
 def call_api(system_prompt, user_prompt, schema):
     response = client.chat.completions.create(
-    model="gpt-5-mini",
-    messages=[
-        {"role": "user", "content": user_prompt},
-        {"role": "system", "content": system_prompt}
-    ],
-    response_format={"type": "json_schema", "json_schema": schema}
+        model="gpt-5-mini",
+        messages=[
+            {"role": "user", "content": user_prompt},
+            {"role": "system", "content": system_prompt},
+        ],
+        response_format={"type": "json_schema", "json_schema": schema},
     )
     text = response.choices[0].message.content
-    return json.loads(text) # type: ignore
+    return json.loads(text)  # type: ignore
 
 
 def main():
     print("app")
-    
-    user_project = input("Describe the project.").strip()
-    user_context = input("Describe the context of the project that you will be working on.").strip()
 
+    user_project = input("Describe the project.").strip()
+    user_context = input(
+        "Describe the context of the project that you will be working on."
+    ).strip()
+
+    # Stage 1: Generate Ideas
     print("Generating possible ideas...\n")
-    ideas = call_api(SYSTEM_PROMPT, 
-                     USER_TEMPLATE.format(project=user_project, context= user_context), 
-                     IDEA_SCHEMA)
+    ideas = call_api(
+        SYSTEM_PROMPT,
+        USER_TEMPLATE.format(project=user_project, context=user_context),
+        IDEA_SCHEMA,
+    )
     print("Summary:")
     print(ideas["summary"])
     print()
@@ -42,17 +49,27 @@ def main():
         idea_num = idea["idea_num"]
         title = idea["label"]
         description = idea["description"]
-        print(f'{idea_num}. {title}')
+        print(f"{idea_num}. {title}")
         print(description)
     chosen_idea_num = input("Pick an option by number")
-    #To do: add error handling for variable
-    chosen_idea = next(idea for idea in ideas["directions"] if idea["id"] == chosen_idea_num), None 
+    # To do: add error handling for variable
+    chosen_idea = (
+        next(idea for idea in ideas["directions"] if idea["id"] == chosen_idea_num),
+        None,
+    )
     if not chosen_idea:
         print("ERROR")
         return
-    #
-
     
+    # Stage 2: Generate Plan
+    print("Generating step by step plan")
+    chosen_json = json.dumps(chosen_idea, ensure_ascii=False)
+    # TODO: CHANGE STRING PROMPTS BELOW
+    plan = call_api(
+        SYSTEM_PROMPT,
+        USER_TEMPLATE.format(project=user_project, context=user_context),
+        CHOSEN_PLAN_SCEMA,
+    )
 
 if __name__ == "__main__":
     main()
--- mvp/prompts.py
+++ mvp/prompts.py
@@ -20,31 +20,72 @@ Return only a JSON that matches the template
 """
 
 IDEA_SCHEMA = {
-  "name": "writers_ideation_output",
-  "schema": {
-    "type": "object",
-    "properties": {
-      "summary": {"type": "string"},
-      "directions": {
-        "type": "array",
-        "items": {
-          "type": "object",
-          "properties": {
-            "idea_num": {"type": "string"},
-            "label": {"type": "string"},
-            "description": {"type": "string"},
-            "sample_prompt": {"type": "string"},
-            "sample_outline": {"type": "string"}
-          },
-          "required": ["idea_num", "label", "description", "sample_prompt", "sample_outline"],
-          "additionalProperties": False
-        }
-      }
+    "name": "writers_ideation_output",
+    "schema": {
+        "type": "object",
+        "properties": {
+            "summary": {"type": "string"},
+            "directions": {
+                "type": "array",
+                "items": {
+                    "type": "object",
+                    "properties": {
+                        "idea_num": {"type": "string"},
+                        "label": {"type": "string"},
+                        "description": {"type": "string"},
+                        "sample_prompt": {"type": "string"},
+                        "sample_outline": {"type": "string"},
+                    },
+                    "required": [
+                        "idea_num",
+                        "label",
+                        "description",
+                        "sample_prompt",
+                        "sample_outline",
+                    ],
+                    "additionalProperties": False,
+                },
+            },
+        },
+        "required": ["summary", "directions"],
+        "additionalProperties": False,
     },
-    "required": ["summary", "directions"],
-    "additionalProperties": False
-  },
-  "strict": True
+    "strict": True,
 }
 
+USER_PLAN = """
+User project: {project} 
+User context: {context}
+Chosen idea: {chosen_idea}
 
+Task:
+1) Use the chosen idea to define end goal
+2) Create a realistic plan based on the chosen idea with ordered steps
+Return only JSON that matches the schema
+"""
+
+CHOSEN_PLAN_SCEMA = {
+    "name": "writers_plan_output",
+    "schema": {
+        "type": "object",
+        "properties": {
+            "chosen_direction_id" : {"type": "string"},
+            "goal": {"type": "string"},
+            "timeline_days": {"type": "int", "minimum": 1},
+            "steps": {
+                "type": "array",
+                "items": "object",
+                "properties": {
+                    "step_number": {"type": "int"},
+                    "label": {"type": "string"},
+                    "instructions": {"type": "string"},
+                },
+                "required": ["step_number", "label", "instructions"],
+                "additionalProperties": False,
+            },
+        },
+        "required": ["goal", "chosen_direction_id", "timeline_days", "steps"],
+        "additionalProperties": False,
+    },
+    "strict": True
+}
